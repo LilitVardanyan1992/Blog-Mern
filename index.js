a3720c9a1a1800cd4d9918fa7cf50792af6bed75
LilitVardanyan1992 <lvardanyan890@gmail.com>
2023-11-01 00:47:49 -0700

    made logic for authorization of user in server: routers - /auth/register, /auth/login, /auth/me

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 import express from "express";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 import mongoose from "mongoose";
 import { registerValidation } from "./validations/auth.js";
 import { validationResult } from "express-validator";
+import UserModal from "./models/User.js";
+import checkAuth from "./utils/checkauth.js";
 
 mongoose
     .connect(
-        "mongodb+srv://lvardanyan890:Pass321@cluster0.kzh9gml.mongodb.net/?retryWrites=true&w=majority"
+        // "mongodb+srv://lvardanyan890:Pass321@cluster0.kzh9gml.mongodb.net/"
+        "mongodb+srv://lvardanyan890:Pass321@cluster0.kzh9gml.mongodb.net/blog?retryWrites=true&w=majority"
     )
     .then(() => console.log("DB ok"))
     .catch((err) => console.log(`DB error: ${err}`));
@@ -15,18 +19,113 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("Hello world");
+app.post("/auth/login", async (req, res) => {
+    try {
+        const user = await UserModal.findOne({ email: req.body.email });
+
+        if (!user) {
+            return res.status(404).json({ message: "User didn't find." });
+        }
+
+        const isValidPass = await bcrypt.compare(
+            req.body.password,
+            user._doc.passwordHash
+        );
+
+        if (!isValidPass) {
+            return res.status(400).json({
+                message: "Invalid login and password.",
+            });
+        }
+
+        const token = jwt.sign(
+            {
+                _id: user._id,
+            },
+            "secret123",
+            {
+                expiresIn: "30d",
+            }
+        );
+
+        const { passwordHash, ...userData } = user._doc;
+
+        res.json({
+            ...userData,
+            token,
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: "Can not authorize.",
+        });
+    }
 });
 
-app.post("/auth/register", registerValidation, (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json(errors.array());
+app.post("/auth/register", registerValidation, async (req, res) => {
+    try {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(400).json(errors.array());
+        }
+
+        const password = req.body.password;
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(password, salt);
+
+        const doc = new UserModal({
+            email: req.body.email,
+            fullName: req.body.fullName,
+            avatarUrl: req.body.avatarUrl,
+            passwordHash: hash,
+        });
+
+        const user = await doc.save();
+
+        const token = jwt.sign(
+            {
+                _id: user._id,
+            },
+            "secret123",
+            {
+                expiresIn: "30d",
+            }
+        );
+
+        const { passwordHash, ...userData } = user._doc;
+
+        res.json({
+            ...userData,
+            token,
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: "Can not register.",
+        });
     }
-    res.json({
-        success: true,
-    });
+});
+
+app.get("/auth/me", checkAuth, async (req, res) => {
+    try {
+        const user = await UserModal.findById(req.userId);
+
+        if (!user) {
+            return res.status(404).json({
+                message: "User didn't find,",
+            });
+        }
+
+        const { passwordHash, ...userData } = user._doc;
+
+        res.json({
+            userData,
+        });
+    } catch (error) {
+        return res.status(404).json({
+            message: "net dostupa",
+        });
+    }
 });
 
 app.listen(4445, (err) => {
